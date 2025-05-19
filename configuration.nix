9dc3cfb57310e571781d3e9c3b961d7b51a3c760
Hugo Allainé <hugo+github@allaine.cc>
2025-05-19 15:03:48 +0200

    first commit

---
+++ configuration.nix
@@ -0,0 +1,99 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, pkgs-unstable, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev";
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "laptop-nixos-allaine-cc";
+  networking.networkmanager.enable = true;
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # Power Management
+  services.power-profiles-daemon.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "fr_FR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hallaine = {
+    isNormalUser = true;
+    description = "Hugo Allainé";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable nix flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  # Packages
+  environment.systemPackages = with pkgs; [
+    cifs-utils
+  ];
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    font-awesome
+    noto-fonts-emoji
+  ];
+
+  fileSystems."/mnt/NAS/hallaine" = {
+    device = "//192.168.1.100/Utilisateurs/hallaine";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smbcredentials"];
+  };
+
+  programs.hyprland.enable = true;
+
+  security.pam.services.login.enableGnomeKeyring = true;
+
+  programs.steam.enable = true;
+
+  system.stateVersion = "24.11";
+}
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Hugo Allainé | Laptop NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@ { self, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
+    nixosConfigurations = {
+      
+      laptop-nixos-allaine-cc = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        
+        specialArgs = { 
+          pkgs-unstable = import nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+        
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.hallaine = import ./home.nix;
+          }
+        ];
+      };
+    
+    };
+  };
+}
