d08adebecfe640c6e7f2a3f5e626f99aac6a364e
hugoallaine <hugo+github@allaine.cc>
2025-10-20 08:05:58 +0200

    changed window opacity

--- home.nix
+++ home.nix
@@ -161,8 +161,8 @@ in
 
       decoration = {
         "rounding" = 10;
-        "active_opacity" = 0.9;
-        "inactive_opacity" = 0.7;
+        "active_opacity" = 0.95;
+        "inactive_opacity" = 0.9;
         shadow = {
           enabled = true;
           "range" = 4;
