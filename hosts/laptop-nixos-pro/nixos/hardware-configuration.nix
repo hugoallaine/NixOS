470c13f24741ad5037194ff3416233f5b04b887b
HugoPlayer <hugo+github@allaine.cc>
2026-01-27 23:10:26 +0100

    fix ;

--- hosts/laptop-nixos-pro/nixos/hardware-configuration.nix
+++ hosts/laptop-nixos-pro/nixos/hardware-configuration.nix
@@ -2,7 +2,7 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ]
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.initrd.extraModulePackages = [ ];
 
