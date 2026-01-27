3f575f3531f803964481ccdaeb69c3ae1492aead
HugoPlayer <hugo+github@allaine.cc>
2026-01-28 00:13:02 +0100

    removed uwsm things for gnome

--- hosts/laptop-nixos-pro/home/default.nix
+++ hosts/laptop-nixos-pro/home/default.nix
@@ -4,7 +4,6 @@
     ../../../modules/home/users/hallaine_hm.nix
     ../../../modules/home/environment/path.nix
     ../../../modules/home/environment/vars.nix
-    ../../../modules/home/config/window_manager/uwsm.nix
     ../../../modules/home/config/shell/bash.nix
     ../../../modules/home/config/shell/kitty.nix
     ../../../modules/home/packages/cli.nix
