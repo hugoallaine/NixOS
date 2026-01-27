9838ee28291e0ef1807e62c80d1ec68e45e3687f
HugoPlayer <hugo+github@allaine.cc>
2026-01-27 23:13:27 +0100

    fix extraModulePackages

--- hosts/laptop-nixos-pro/nixos/hardware-configuration.nix
+++ hosts/laptop-nixos-pro/nixos/hardware-configuration.nix
@@ -4,7 +4,7 @@
 
   boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.initrd.extraModulePackages = [ ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/1bd6a941-84d0-4891-a988-0cf01222c927";
