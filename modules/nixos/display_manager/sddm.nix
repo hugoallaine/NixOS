18baaeab2c6b59881ad6d15e36aca515c0cb46d0
HugoPlayer <hugo+github@allaine.cc>
2026-01-28 01:31:06 +0100

    commented sddm packages

--- modules/nixos/display_manager/sddm.nix
+++ modules/nixos/display_manager/sddm.nix
@@ -4,7 +4,7 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    package = pkgs.kdePackages.sddm;
+    #package = pkgs.kdePackages.sddm;
     theme = "minesddm";
   };
 }
